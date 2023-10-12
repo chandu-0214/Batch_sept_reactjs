1d01dc785c22145d8a76619eaff30a0f876499aa
chandu-0214 <ncr02141997@gmail.com>
2023-10-12 18:55:50 +0530

    first commit

--- src/components/state and events/forms.js
+++ src/components/state and events/forms.js
@@ -1,22 +1,33 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 function InputForm() {
     const inputRef=useRef(null)
+    const [inputval, setInputval] = useState('')
     const handleINput=(event)=>{
-            console.log("Input triggered")
+            setInputval(event.target.value)
             
     }
-    const handleClick=(event)=>{
-        console.log("Click triggered")
-        console.log(inputRef)
-        inputRef.current="chandu"
-}
+    const handleClick = ()=>{
+      console.log("Click is triggered")
+      // console.log(inputRef.current)
+      inputRef.current.focus()
+      inputRef.current.value = 'chandu'
+    }
+
+    useEffect(() => {
+      inputRef.current=inputval
+      console.log(inputRef.current)
+    },[inputval])
+    
+
   return (
     <div>
         <h2>Use Ref</h2>
-        {console.log(inputRef.current)}
-        <input value={inputRef.current} ref={inputRef} onChange={handleINput} type='text' placeholder='write anything'/>
+        {console.log('render')}
+        <input ref={inputRef} value={inputval}onChange={handleINput} type='text' placeholder='write anything'/>
         <button onClick={handleClick}>Click</button>
+        <h2>Current Value : {inputval}</h2>
+        <h2>Previous Value :{inputRef.current}</h2>
     </div>
   )
 }
